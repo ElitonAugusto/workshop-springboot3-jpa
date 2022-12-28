93cb159e022a11bf466ec923b2c2e72f39a0ebe0
Eliton <eliton.dev.25@gmail.com>
2022-12-28 14:19:40 -0300

    Payment, one-to-one association

--- src/main/java/com/projetospringboot/aula/config/TestConfig.java
+++ src/main/java/com/projetospringboot/aula/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetospringboot.aula.entities.Category;
 import com.projetospringboot.aula.entities.Order;
 import com.projetospringboot.aula.entities.OrderItem;
+import com.projetospringboot.aula.entities.Payment;
 import com.projetospringboot.aula.entities.Product;
 import com.projetospringboot.aula.entities.User;
 import com.projetospringboot.aula.entities.unums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPeyment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
