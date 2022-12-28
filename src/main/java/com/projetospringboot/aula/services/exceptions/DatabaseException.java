1a7d3bba6b346b5741b0112a8fa1ac900408a85e
Eliton <eliton.dev.25@gmail.com>
2022-12-28 16:19:30 -0300

    Exception handling - delete

---
+++ src/main/java/com/projetospringboot/aula/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.projetospringboot.aula.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException (String msg) {
+		super(msg);
+	}
+}
